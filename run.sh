45817ca96bd37af6d92b11c6264ac0152dd7ccb9
the8472 <the8472@58cabe8c-710f-0410-8395-bf4839b10e17>
2010-11-30 16:30:35 +0000

    git-svn-id: http://svn.code.sf.net/p/azsmrc/code/mldht/trunk@1975 58cabe8c-710f-0410-8395-bf4839b10e17

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-java -Xmx2048m -XX:NewSize=20m -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:GCTimeRatio=19 -XX:MinHeapFreeRatio=10 -XX:MaxHeapFreeRatio=30  -cp ".:libs/*:DHTIndexer.jar:libs/hibernate/*" lbms.plugins.mldht.indexer.Main
+cd work
+java -Xmx2048m -XX:NewSize=20m -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:GCTimeRatio=19 -XX:MinHeapFreeRatio=10 -XX:MaxHeapFreeRatio=30  -cp ".:../libs/*:../DHTIndexer.jar:../libs/hibernate/*:.." lbms.plugins.mldht.indexer.Main

a4c6e07d431108d71daab98af4febfb1fee3bef5
The8472 <git@infinite-source.de>
2015-04-26 02:23:07 +0200

    skip bucket if there's nothing to insert

--- src/lbms/plugins/mldht/kad/KClosestNodesSearch.java
+++ src/lbms/plugins/mldht/kad/KClosestNodesSearch.java
@@ -1,25 +1,27 @@
 /*
- *    This file is part of mlDHT. 
+ *    This file is part of mlDHT.
  * 
- *    mlDHT is free software: you can redistribute it and/or modify 
- *    it under the terms of the GNU General Public License as published by 
- *    the Free Software Foundation, either version 2 of the License, or 
- *    (at your option) any later version. 
+ *    mlDHT is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 2 of the License, or
+ *    (at your option) any later version.
  * 
- *    mlDHT is distributed in the hope that it will be useful, 
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of 
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- *    GNU General Public License for more details. 
+ *    mlDHT is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
  * 
- *    You should have received a copy of the GNU General Public License 
- *    along with mlDHT.  If not, see <http://www.gnu.org/licenses/>. 
+ *    You should have received a copy of the GNU General Public License
+ *    along with mlDHT.  If not, see <http://www.gnu.org/licenses/>.
  */
 package lbms.plugins.mldht.kad;
 
 import java.net.InetSocketAddress;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
-import lbms.plugins.mldht.kad.DHT.DHTtype;
 import lbms.plugins.mldht.kad.Node.RoutingTableEntry;
 import lbms.plugins.mldht.kad.utils.PackUtil;
 
@@ -71,15 +73,10 @@ public class KClosestNodesSearch {
 	 * @return true if we're done
 	 */
 	private boolean insertBucket(KBucket bucket) {
+		if(bucket.getNumEntries() == 0)
+			return false;
 		Key farthest = entries.size() > 0 ? entries.get(entries.size()-1).getID() : null;
-		List<KBucketEntry> bucketEntries = bucket.getEntries();
-		KBucketEntry e;
-		for(int i=0,n=bucketEntries.size();i<n;i++)
-		{
-			e = bucketEntries.get(i);
-			if(!e.isBad())
-				entries.add(e);
-		}
+		bucket.entriesStream().filter(e -> !e.isBad()).forEach(entries::add);
 		Collections.sort(entries,comp);
 		for(int i=entries.size()-1;i>=max_entries;i--)
 			entries.remove(i);

910cdc3f42ca569e7b9684a7a622bdcde4996198
The8472 <git@infinite-source.de>
2015-04-27 02:51:02 +0200

    can't really encode a whole battery of statistical tests, but probing the distribution of hamming weights is easy enough

--- test/the8472/test/bencode/KeyTest.java
+++ test/the8472/test/bencode/KeyTest.java
@@ -1,9 +1,12 @@
 package the8472.test.bencode;
 
 import static org.junit.Assert.assertEquals;
-import lbms.plugins.mldht.kad.Key;
+
+import java.util.stream.IntStream;
 
 import org.junit.Test;
+
+import lbms.plugins.mldht.kad.Key;
 
 public class KeyTest {
 
@@ -12,6 +15,16 @@ public class KeyTest {
 		assertEquals(Key.MIN_KEY.compareTo(Key.MAX_KEY), -1);
 		assertEquals(Key.MAX_KEY.compareTo(Key.MIN_KEY), 1);
 		assertEquals(Key.MAX_KEY.compareTo(Key.MAX_KEY), 0);
+	}
+	
+	@Test
+	public void testHashCode() {
+		
+		int hammingWeights[] = IntStream.range(0, 256).map(i -> Integer.bitCount(Key.createRandomKey().hashCode())).sorted().toArray();
+		int median = hammingWeights[hammingWeights.length/2];
+		
+		assertEquals(median, 16);
+		
 	}
 
 }

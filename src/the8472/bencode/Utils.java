3a0d41accab1ea383a7909fcffe2220f3972da59
The8472 <git@infinite-source.de>
2015-07-22 04:04:52 +0200

    add tests for BEP33 test vectors

--- src/the8472/bencode/Utils.java
+++ src/the8472/bencode/Utils.java
@@ -165,6 +165,15 @@ public class Utils {
 	}
 	
 	
+	static byte[] hex2ary(CharSequence hex) {
+		byte[] bytes = new byte[hex.length()/2];
+		
+	    for (int i = 0; i < hex.length(); i += 2)
+	        bytes[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4) + Character.digit(hex.charAt(i+1), 16));
+	    
+	    return bytes;
+	}
+	
 
 
 }

6548288ef1000a0417bd818c6a0f9e9de689059e
The8472 <git@infinite-source.de>
2015-09-26 00:18:13 +0200

    update cli help

--- src/the8472/mldht/cli/commands/Help.java
+++ src/the8472/mldht/cli/commands/Help.java
@@ -6,9 +6,11 @@ public class Help extends CommandProcessor {
 
 	@Override
 	protected void process() {
-		println("HELP                      - prints this help");
-		println("PING ip port              - continuously pings a DHT node with a 1 second interval");
-		println("GETTORRENT infohash path  - perform peer lookup for <infohash>, then attempt metadata exchange, then write .torrent to <path>");
+		println("HELP                                - prints this help");
+		println("PING ip port                        - continuously pings a DHT node with a 1 second interval");
+		println("GETTORRENT infohash [infohash ...]  - peer lookup for <infohash(es)>, then attempt metadata exchange, then write .torrent file(s) to the current working directory");
+		println("GETPEERS infohash [infohash ...]    - peer lookup for <infohash(es)>, print ip address/port tuples");
+		println("BURST [count]                       - run a batch of find_node lookups to random target IDs. intended test the attainable throughput for active lookups, subject to internal throttling");
 		exit(0);
 	}
 
